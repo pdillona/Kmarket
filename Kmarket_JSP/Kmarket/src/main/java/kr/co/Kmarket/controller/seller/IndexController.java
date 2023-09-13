883a2b1c8e1ed298e79240a0b9e9400161b85b87
unknown <wjdals1217@gmail.com>
2023-09-13 18:27:38 +0900

    Kmarket_JSP seller 수정

--- Kmarket_JSP/Kmarket/src/main/java/kr/co/Kmarket/controller/seller/IndexController.java
+++ Kmarket_JSP/Kmarket/src/main/java/kr/co/Kmarket/controller/seller/IndexController.java
@@ -17,6 +17,7 @@ public class IndexController extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/seller/index.jsp");
 		dispatcher.forward(req, resp);
 	}
