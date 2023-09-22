628481f6ceeaff10520ef0bcde4a024416ff46ff
z82714567“git config --global user.name z82714567“ <z82714567@gmail.com>
2023-09-22 12:12:06 +0900

    member loginController 주석

--- Kmarket_JSP/Kmarket/src/main/java/kr/co/Kmarket/controller/member/LoginController.java
+++ Kmarket_JSP/Kmarket/src/main/java/kr/co/Kmarket/controller/member/LoginController.java
@@ -61,9 +61,9 @@ public class LoginController extends HttpServlet{
             logger.info("dto :" + dto);
             //자동로그인 처리(체크박스 체크했을 때)
 			if(auto != null) {
-				Cookie autoCookie = new Cookie("cid", dto.getUid());
-				autoCookie.setMaxAge(60*60*24*7); //일주일 (초, 분, 시, 일)
-				autoCookie.setPath("/");
+				Cookie autoCookie = new Cookie("cid", session.getId()); //쿠키에 dto(보안상의 문제)를 저장하는 것이 아니고 브라우저의 세션 id를 저장함
+				autoCookie.setMaxAge(60*60*24*7); //7일 (초, 분, 시, 일) (브라우저 종료와 관계없이 - 쿠키니깐!)
+				autoCookie.setPath("/"); //contextPath 이하 모든 요청에 대해 쿠키를 전송할 수 있음
 				resp.addCookie(autoCookie);
 				logger.info("autoC : " + autoCookie.getValue());
 				logger.info("autoC : " + autoCookie.getName());
